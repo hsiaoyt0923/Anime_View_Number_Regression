2164e459c462cb91e2153556f0f4ef05adce3123
Eddie Hsiao <xcvb1321@gmail.com>
2023-12-02 17:17:23 +0800

    modify

--- datasource.py
+++ datasource.py
@@ -13,7 +13,9 @@ def __download_data(url: str) -> list[list]:
 
     # 建立隨機referer
     referer_choices = ['https://jplop.neocities.org/shar_free_movie_site',
-                       'www.google.com', 'https://favsk.com/ani-gamer/', 'www.bing.com']
+                       'https://www.google.com',
+                       'https://favsk.com/ani-gamer/',
+                       'https://www.bing.com'] + [f'https://ani.gamer.com.tw/animeList.php?page={i+2}' for i in range(56)]
     referer = random.choice(referer_choices)
 
     # 開始爬蟲
@@ -24,11 +26,18 @@ def __download_data(url: str) -> list[list]:
         print(f'請求成功：{response.status_code}')
     else:
         print(f'請求失敗：{response.status_code}')
+
+    # 建立BeautifulSoup物件
     intro_data = BeautifulSoup(response.text, 'html.parser')
     anime_infos = intro_data.select('.theme-list-main')
+
+    # 最後傳出的list
     anime_data = []
+
+    # 使用迴圈從目標區塊取得資訊
     for anime_info in anime_infos:
         anime_name = anime_info.select_one('.theme-name').text.strip()
+        # 區分全年齡版與年齡限制版
         if anime_info.select_one('.color-R18') is not None:
             anime_name = anime_name + ' 年齡限制版'
         show_view_number = anime_info.select_one(
@@ -44,14 +53,19 @@ def __download_data(url: str) -> list[list]:
         delay = random.choice(delay_choices)  # 隨機選取秒數
         time.sleep(delay)
 
+        # 進入作品播放頁面取得詳細資訊
         r1 = requests.get(anime_link, headers={
-                          "User-Agent": user_agent.random, "Referer": referer, })
+            "User-Agent": user_agent.random, "Referer": referer, })
         r1.encoding = 'utf8'
+
+        # 建立BeautifulSoup物件
         detail_data = BeautifulSoup(r1.text, 'html.parser')
         acg_score = detail_data.select_one('.acg-score')
         star = acg_score.select_one('.score-overall-number').text.strip()
         rating_people = acg_score.select_one(
             '.score-overall-people').text.strip().replace('人評價', '').replace(',', '')
+
+        # 處理staff和tag
         type_list = detail_data.select_one('.type-list')
         staff = []
         tags = []
@@ -60,12 +74,20 @@ def __download_data(url: str) -> list[list]:
             staff.append(p.text)
         for li in type_list.select('.tag'):
             tags.append(li.text)
+
+        # 建立一個list儲存除tag外所有資訊
         infos = [anime_name, show_view_number, anime_time, anime_episode,
                  anime_link, star, rating_people, staff[1], staff[2], staff[3]]
+
+        # pre_data裡有一部作品所有資訊
         pre_data.append(infos)
         pre_data.append(tags)
+
+        # anime_data裡有所有作品的pre_data
         anime_data.append(pre_data)
+
         # print(f'動畫名:{anime_name}\n觀看數:{show_view_number}\n季度:{anime_time}\n集數:{anime_episode}\n動畫連結:{anime_link}\n{star}\n{rating_people}\n導演:{staff[1]}\n代理商:{staff[2]}\n製作廠商:{staff[3]}\n分類:{tags}\n')
+
     return anime_data
 
 
