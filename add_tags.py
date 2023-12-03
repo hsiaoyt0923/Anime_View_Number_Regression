ef7e5ffeffb69d0a396c8bfc7d07b047a0b316aa
Eddie Hsiao <xcvb1321@gmail.com>
2023-12-03 12:53:17 +0800

    modify

--- add_tags.py
+++ add_tags.py
@@ -25,29 +25,51 @@ def download_tags(url: str) -> list[list]:
     anime_tags = []
 
     for anime_content in anime_contents:
+        per_anime = []
         anime_name = anime_content.select_one('.entity_localized_name').text
-        tag_list = [anime_name]
+        other_names = anime_content.select_one('.anime_summary > i')
+        if other_names is None:
+            other_names = ''
+        else:
+            other_names = other_names.text.replace('其他名稱：', '')
+        name_list = [anime_name] + other_names.split('、')
         tags = anime_content.select('.anime_tag > tags')
+        tag_list = []
         for tag in tags:
             tag_list.append(tag.text)
-        tag_list = [tag_list[i] for i in range(3)]
-        anime_tags.append(tag_list)
-    print(anime_tags)
+        tag_list = [tag_list[i] for i in range(2)]
+        per_anime.append(name_list)
+        per_anime.append(tag_list)
+        anime_tags.append(per_anime)
     return anime_tags
 
 
-def insert_tags(conn, values: list[str]):
-    values[0] = values[0].replace("'", "")
-    values[0] = values[0].replace("2", "二")
+def insert_tags(conn, tags: list[list]):
+    name_list = tags[0]
+    tag_list = tags[1]
+    f_list = name_list[0].split()
+    fistname = f_list[0].replace("'", '')
+    namestr_list = []
+    for item in name_list:
+        strip_item = item.replace("'", '')
+        namestr_list.append(f"'{strip_item}'")
+    namestr = ''
+    for item in namestr_list:
+        namestr += f'{item}'
+        namestr += ','
+
+    namestr = namestr.rstrip(',')
+
+    print(namestr)
+    print(fistname)
     sql = f'''
-        update 巴哈姆特動畫瘋
-        set 原作載體='{values[1]}', 新續作='{values[2]}'
-        where 動畫名='{values[0]}'
+        update 動畫瘋訓練資料集
+        set 原作載體='{tag_list[0]}', 新續作='{tag_list[1]}'
+        where 動畫名 in ({namestr}) or 動畫名 like '%{fistname}%'
         '''
     cursor = conn.cursor()
     cursor.execute(sql)
     cursor.close()
-    conn.commit()
 
 
 def main():
@@ -62,11 +84,31 @@ def main():
         for j in ['01', '04', '07', '10']:
             url = f'https://acgsecrets.hk/bangumi/{i+j}/'
             anime_tags = download_tags(url)
+            print(f'{i+j}新番下載成功')
             for anime_tag in anime_tags:
-                insert_tags(conn, anime_tag)
+                insert_tags(conn, tags=anime_tag)
+                conn.commit()
 
     conn.close()
 
 
+def fetch_data(conn) -> list[tuple]:
+    select_query = '''
+         * FROM 巴哈姆特動畫瘋
+        WHERE 季度 LIKE '2020%' OR
+        季度 LIKE '2021%' OR
+        季度 LIKE '2022%' OR
+        季度 in ('2023/01','2023/04')
+        order by id
+        '''
+    cursor = conn.cursor()
+    cursor.execute(select_query)
+    rows = cursor.fetchall()
+    cursor.close()
+    return rows
+
+
 if __name__ == '__main__':
     main()
+
+
