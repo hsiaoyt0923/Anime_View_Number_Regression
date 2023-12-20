1b0d6e8e87abb6c3c4cb4733a5b4ed74572b1049
Eddie Hsiao <xcvb1321@gmail.com>
2023-12-20 15:36:40 +0800

    modify

--- dash_web/pages/analytics.py
+++ dash_web/pages/analytics.py
@@ -8,13 +8,14 @@ dash.register_page(__name__)
 
 df1 = pd.read_csv('./web_csv/genre_table.csv')
 df2 = pd.read_csv('./web_csv/Tags_View_Number.csv')
-df3 = pd.read_csv('./web_csv/genre2_table.csv')
-df4 = pd.read_csv('./web_csv/Anime_Company.csv')
+df3 = pd.read_csv('./web_csv/Anime_Company.csv')
+df4 = pd.read_csv('./web_csv/genre2_table.csv')
+
 
 
 layout = html.Div([
     html.H1(children='影響觀看數因子', style={'textAlign':'center'}),
-    dcc.Dropdown(['作品分類(全部)','作品分類(代表性)','原創改編、新續作','動畫公司'], '作品分類(全部)', id='dropdown-selection'),
+    dcc.Dropdown(['作品分類(全部)','作品分類(代表性)','動畫公司','原創改編、新續作'], '作品分類(全部)', id='dropdown-selection'),
     dash_table.DataTable(data=df1.to_dict('records'), page_size=10, id='main_table'),
     dcc.Graph(id='graph-content'),
     
@@ -30,7 +31,7 @@ def csv_toggle(value):
         return df1.to_dict('records'), 
     if value == '作品分類(代表性)':
         return df2.to_dict('records'), 
-    if value == '原創改編、新續作':
-        return df3.to_dict('records'),
     if value == '動畫公司':
+        return df3.to_dict('records'),
+    if value == '原創改編、新續作':
         return df4.to_dict('records'),
